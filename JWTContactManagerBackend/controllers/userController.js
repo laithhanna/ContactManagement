3971b97cf69111239ea2f261a9c4ed9247501333
Laith <laithhanna6@gmail.com>
2023-11-05 12:18:17 -0500

    User Registration and Password Hashing

--- JWTContactManagerBackend/controllers/userController.js
+++ JWTContactManagerBackend/controllers/userController.js
@@ -1,10 +1,37 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "User registered" });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  //check if user already in the DB
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered!");
+  }
+
+  //Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
+  res.json({ message: "User registered successfully" });
 });
 
 //@desc Login a user
