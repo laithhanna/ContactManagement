7e69181bcaff26f7a3e752ec2219cf6b34456294
Laith <laithhanna6@gmail.com>
2023-11-04 22:46:57 -0400

    Connect Express App to MongoDB Database

--- JWTContactManagerBackend/server.js
+++ JWTContactManagerBackend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection");
 const contactRoutes = require("./routes/contactRoutes");
 const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
